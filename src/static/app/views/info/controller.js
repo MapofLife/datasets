12d81461ecb4410d9d1578b55aa8760421063ab7
Jeremy Malczyk <jmalczyk@gmail.com>
2016-06-07 17:24:58 -0400

    Removing detritus

---
+++ src/static/app/views/info/controller.js
@@ -0,0 +1,6 @@
+angular.module('mol.controllers')
+ .controller('molDatasetsInfoCtrl',
+    ['$scope',
+    function($scope) {
+        //Do something
+    }]);
---
+++ src/static/app/views/list/controller.js
@@ -0,0 +1,11 @@
+angular.module('mol.controllers')
+  .controller('molDatasetsListCtrl',
+  	[
+      '$window','$http','$uibModal','$scope', '$state', '$filter',
+      '$timeout','$location','$anchorScroll','$q','molApi','uiGmapGoogleMapApi',
+   		function(
+         $compile, $window, $http, $modal, $scope, $state, $filter,
+          $timeout, $location, $anchorScroll, $q,  molApi,uiGmapGoogleMapApi) {
+
+
+  }]);
