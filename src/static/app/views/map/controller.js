28a28b18515c32ae6600eb5d6eb6f65adc4496ea
Jeremy Malczyk <jmalczyk@gmail.com>
2016-07-06 16:13:04 -0400

    Updating build

--- src/static/app/views/map/controller.js
+++ src/static/app/views/map/controller.js
@@ -1,6 +1,6 @@
 angular.module('mol.controllers').controller('molDatasetsMapCtrl',
-    ['$scope', 'leafletData', '$timeout', '$window', '$http', '$filter', 'molApi','$q','$state',
-    function($scope, leafletData, $timeout, $window, $http, $filter, molApi,$q,$state) {
+    ['$scope', 'leafletData', 'leafletBoundsHelpers','$timeout', '$window', '$http', '$filter', 'molApi','$q','$state',
+    function($scope, leafletData, leafletBoundsHelpers,$timeout, $window, $http, $filter, molApi,$q,$state) {
 
 
   $scope.$watch('model.choices', function() {
@@ -9,6 +9,9 @@ angular.module('mol.controllers').controller('molDatasetsMapCtrl',
 
   $scope.map = {
     center: { lat: 0, lng: 0, zoom: 3 },
+    extent: leafletBoundsHelpers.createBoundsFromArray([
+      [90,180],[-90,-180]
+    ]),
     events: { map: { enable: ['click'], logic: 'emit' } },
     controls: {
       fullscreen: {
@@ -41,13 +44,13 @@ angular.module('mol.controllers').controller('molDatasetsMapCtrl',
 
   $scope.canceller.resolve();
     $scope.canceller = $q.defer();
-      $http({
-        "url": "https://mol.cartodb.com/api/v1/map/named/datasets-map",
-        "method": "POST",
-        "data" : payload,
-        "timeout":   $scope.canceller
+      molApi({
+        "service": "inventory/maps",
+        "params" : payload,
+        "canceller":   $scope.canceller,
+        "loading":true
       }).then(function(response) {
-      var result = response.data, tile_url = ""+
+      /*var result = response.data, tile_url = ""+
           "https://{0}/mol/api/v1/map/{1}/{z}/{x}/{y}.png"
             .format(result.cdn_url.https,
 
@@ -56,18 +59,25 @@ angular.module('mol.controllers').controller('molDatasetsMapCtrl',
           "https://{0}/mol/api/v1/map/{1}/0/{z}/{x}/{y}.grid.json"
             .format(
               result.cdn_url.https,
-              result.layergroupid);
+              result.layergroupid);*/
 
+       var bounds =  leafletBoundsHelpers.createBoundsFromArray([
+         response.data.extent.coordinates[0][2].reverse(),
+         response.data.extent.coordinates[0][0].reverse(),
+       ]);
+       $scope.map.bounds = bounds;
        $scope.map.layers.overlays = {
          xyz: {
            name: 'Datasets',
            visible: true,
-           url: tile_url,
+           url: response.data.tile_url,
            type: 'xyz',
            doRefresh: true
          }
        };
       });
+
+
 
   };
 
