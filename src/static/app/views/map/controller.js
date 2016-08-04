25e1bc18baead6922c8a76137c2bfaf4beac98ef
rafe <raphael.lafrance at gmail.com>
2016-08-04 09:16:58 -0400

    Add map overlay toggles #27 #23

--- src/static/app/views/map/controller.js
+++ src/static/app/views/map/controller.js
@@ -16,34 +16,69 @@ angular.module('mol.controllers').controller('molDatasetsMapCtrl',
     defaults: { minZoom: 2 },
     bounds: {},
     legend: {},
+    legends: {},
     layers: {
       baselayers: {
         xyz: {
           name: 'OpenStreetMap (XYZ)',
           url: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          type: 'xyz'
+          type: 'xyz',
+          layerParams: { showOnSelector: false },
+          layerOptions: { showOnSelector: false },
         }
-      }
+      },
+      overlays: {},
     },
   };
 
   $scope.canceller = $q.defer();
 
-  $scope.datasetsQuery = function() {
-    var payload = {};
-    Object.keys($scope.model.choices).forEach(function (facet) {
-      var choices = $scope.model.choices[facet];
-      payload[facet] = Object.keys(choices).filter(function(choice) {
-        return choices[choice]
-      }).join(',').toLowerCase() || '';
-    });
-
-    if ($state.params.dataset) {
-      payload = { dataset_id: $state.params.dataset };
+  $scope.showOverlay = function(overlay) {
+    if (overlay) {
+      $scope.overlay = overlay;
     }
+    Object.keys($scope.map.legends).forEach(function(name) {
+      $scope.setLegend($scope.overlay);
+      $scope.map.layers.overlays[name].visible = name == $scope.overlay;
+    });
+  };
+
+  $scope.datasetsQuery = function() {
+    var name, payload = {};
 
     $scope.canceller.resolve();
     $scope.canceller = $q.defer();
+
+    if ($state.params.dataset) {
+      name = 'Species Counts';
+      payload = {
+        dataset_id: $state.params.dataset,
+        property: 'no_species',
+        reducer: 'sum'
+      };
+      if (!$scope.layer || $scope.layer == 'Dataset Counts') {
+        $scope.showOverlay(name);
+      }
+    } else {
+      name = 'Dataset Counts';
+      Object.keys($scope.model.choices).forEach(function (facet) {
+        var choices = $scope.model.choices[facet];
+        payload[facet] = Object.keys(choices).filter(function(choice) {
+          return choices[choice]
+        }).join(',').toLowerCase() || '';
+      });
+      if (!$scope.layer || $scope.layer == 'Species Counts') {
+        $scope.showOverlay(name);
+      }
+    }
+    $scope.getLayer(payload, name, true);
+
+    payload.property = 'no_records';
+    payload.reducer = 'sum';
+    $scope.getLayer(payload, 'Record Counts', false);
+  };
+
+  $scope.getLayer = function(payload, name, active) {
     molApi({
       service: 'inventory/maps',
       params : payload,
@@ -51,21 +86,34 @@ angular.module('mol.controllers').controller('molDatasetsMapCtrl',
       processing: true
     }).then(function(response) {
       var padding = 5;
-      $scope.map.bounds = leafletBoundsHelpers.createBoundsFromArray([
-        [response.data.extent.coordinates[0][2][1] + padding, response.data.extent.coordinates[0][2][0] + padding],
-        [response.data.extent.coordinates[0][0][1] - padding, response.data.extent.coordinates[0][0][0] - padding]
-      ]);
-      $scope.map.layers.overlays = {
-        xyz: {
-          name: 'Datasets',
-          visible: true,
-          url: response.data.tile_url,
-          type: 'xyz',
-          doRefresh: true
-        }
+      $scope.map.legends[name] = $scope.buildLegend(response.data.legend);
+      if (active) {
+        $scope.setLegend(name);
+        $scope.updateMapBounds(response.data.extent.coordinates);
+      }
+      $scope.map.layers.overlays[name] = {
+        name: name,
+        type: 'xyz',
+        doRefresh: true,
+        visible: active,
+        url: response.data.tile_url,
+        // layerParams: { showOnSelector: false },
+        // layerOptions: { showOnSelector: false },
       };
-      $scope.map.legend = $scope.buildLegend(response.data.legend);
     });
+  };
+
+  $scope.setLegend = function(name) {
+    $scope.map.legend = $scope.map.legends[name];
+    document.styleSheets[0].addRule('.legend:before', 'content: "' + name + '";');
+  };
+
+  $scope.updateMapBounds = function(coordinates) {
+    var padding = 5;
+    $scope.map.bounds = leafletBoundsHelpers.createBoundsFromArray([
+      [coordinates[0][2][1] + padding, coordinates[0][2][0] + padding],
+      [coordinates[0][0][1] - padding, coordinates[0][0][0] - padding]
+    ]);
   };
 
   $scope.buildLegend = function(legendData) {
@@ -92,7 +140,7 @@ angular.module('mol.controllers').controller('molDatasetsMapCtrl',
       }
       legend.colors.push(item.color);
     });
-    if ($state.params.dataset) {
+    if (legend.labels.length == 2) {
       legend.labels = legend.labels.slice(0, 1);
       legend.colors = legend.colors.slice(0, 1);
     }
